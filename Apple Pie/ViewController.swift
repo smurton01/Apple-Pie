cc1db5a0a80d99bead81cb5042ad5c7f25b7592c
Saya M <saya.murton@gmail.com>
2018-06-08 17:22:58 -0600

    Variables & constants
    -added constant `incorrectMovesAllowed`
    -added variables `totalWins` & `totalLosses`
    -added `newRound()` into viewDidLoad

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     var listOfWords = ["pizza", "cooking", "gaming", "noob", "swift", "japan"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,7 +23,13 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func newRound(){
+        
     }
 
     override func didReceiveMemoryWarning() {
